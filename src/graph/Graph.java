09bcae987986a5c01ca231ccbe9d0bc67ba69b77
vigack <vigack@gmail.com>
2017-11-28 21:12:09 +0800

    update

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -3,6 +3,7 @@ package graph;
 import linear.Bag;
 
 /**
+ * 无向图实现
  * Created by liang on 2017/11/28.
  */
 public class Graph {
@@ -22,6 +23,17 @@ public class Graph {
 
 //    public Graph(InputStream in){}
 
+    // 深复制给定图
+    public Graph(Graph origin){
+        this(origin.V());
+
+        // 遍历所有定点，建立所有的边
+        // 复杂度为 定点数X度
+        for(int i=0;i<V;i++)
+            for(int w : origin.adj(i))
+                this.addEdge(i, w);
+    }
+
     public int V(){
         return V;
     }
@@ -33,13 +45,25 @@ public class Graph {
     public void addEdge(int v, int w){
         adj[v].add(w);
         adj[w].add(v);
-        E++;
+        E++;    // 平行边算同一条
+
+        // 若不允许出现平行边和自环
+        // 则要判断v!=w  ===> 排除自环
+        // 只添加v-w，并且添加之前判断是否存在w-v
     }
 
     public Iterable<Integer> adj(int v){
         return adj(v);
     }
 
+    // 习4.1.4
+    public boolean hasEdge(int v, int w){
+        // 直接遍历其中一者的所有相邻节点
+        for(int k : adj(v))
+            if (k == w)
+                return true;
+        return false;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
